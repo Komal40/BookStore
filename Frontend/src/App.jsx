07e12cc8f2ad15cd5bc2d27bb2b72cfd2bf90714
komal <singh777komal@gmail.com>
2024-05-03 17:11:44 +0530

    made banner and footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+import Footer from './Components/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Banner/>
+      <Footer/>
     </>
   )
 }
